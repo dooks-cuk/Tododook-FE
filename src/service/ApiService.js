021f153c990b4ef8a312583dcafed9568ff6bcdb
sor999 <sorsor999@naver.com>
2024-06-16 16:06:21 +0900

    commet: 주석 수정

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -40,57 +40,7 @@ export function call(api, method, request) {
         });
 }
 
-
-
-
-    // return fetch(options.url, options)
-    //   .then((response) =>
-        
-    //     response.json().then((json) => {
-    //       if (!response.ok) {
-    //        // return response.json().catch(() => ({})); // JSON 파싱 오류 처리 및 빈 객체 반환
-    //         return Promise.reject(json);
-    //       }
-    //       return json;
-
-    //     }
-    //   )
-     
-    //   )
-    //   .catch((error) => {
-    //     console.error("Error during fetch:", JSON.stringify(error));
-    //     if (error.status === 403 || error.status === 401) {
-    //       window.location.href = "/login";
-    //     }
-    //     return Promise.reject(error);
-    //   });
-    
-  // }
-
-// // 로그인을 위한 API 서비스 메소드 signin
-// export async function signin(userDTO) {
-//     try {
-//       const response = await call("/api/auth/login", "POST", userDTO);
-//       console.log(response);
-//       // if (response.code === 400) {
-//       //   throw new Error("아이디 혹은 비밀번호가 틀립니다. 다시 입력해주세요.");
-//       // }
-//       if (response.status === 200) {
-//         return response; // 성공 시 응답 반환
-//       }
-//       // if (response.accessToken) {
-//       //   // local 스토리지에 토큰 저장
-//       //   localStorage.setItem("ACCESS_TOKEN", response.accessToken);
-//       //   // accessToken이 존재하는 경우 todo 화면으로 리다이렉트
-//       //   window.location.href = "/";
-//       // }
-//     } catch (error) {
-//       console.error("Signin failed:", JSON.stringify(error));
-//       throw error;
-//       //return Promise.reject(error);
-//     }
-// }
-
+// 로그인 요청
 
 export async function signin(userDTO) {
   try {
@@ -103,56 +53,6 @@ export async function signin(userDTO) {
     throw error; // 기타 오류는 throw
   }
 }
-
-
-// // 회원 가입 요청
-// export async function signup(userDTO) {
-//     try {
-//       const response = await call("/api/auth/signup", "POST", userDTO);
-//       console.log(response);
-//       // if(response.status === 400) {
-//       //   throw new Error("이미 존재하는 이메일입니다.");
-//       // }
-
-//       if(response.status === 201) {
-//         window.location.href = "/";
-//       }
-
-//     }
-//     catch (error) {
-//       console.error("Signup failed:", JSON.stringify(error));
-//       if (error.status === 403) {
-//           window.location.href = "/signup";
-//       }
-//       // else if (error.status === 400) {
-//       //   throw new Error("이미 존재하는 이메일입니다.");
-//       // } else {
-//       //   console.error(`추가 오류 정보: ${error.message}`);
-//       // }
-//       console.error(error);
-//     }
-// }
-// 회원 가입 요청
-
-
-// export async function signup(userDTO) {
-//   try {
-//     const response = await call("/api/auth/signup", "POST", userDTO);
-//     console.log(response);
-
-//     if (response.status === 201) {
-//       window.location.href = "/";
-//     }
-
-//   } catch (error) {
-//     console.error("Signup failed:", JSON.stringify(error));
-//     if (error.status === 400) {
-//       return Promise.reject(error.errors); // 400 오류 시 필드별 오류 메시지를 반환
-//     }
-//     return Promise.reject(error);
-//   }
-// }
-
 
 // 회원 가입 요청
 export async function signup(userDTO) {
@@ -172,7 +72,6 @@ export async function signup(userDTO) {
       return Promise.reject(error);
   }
 }
-
 
 // 로그아웃
 export function signout() {
